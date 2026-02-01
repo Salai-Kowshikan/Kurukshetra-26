03ec321ca08e833512d926c9d68f976a1889b196
Kulasekaran <kulasekaranm11@gmail.com>
2026-02-01 18:05:29 +0530

    New Navbar design implemented using Zustand

--- k-site/src/pages/Home.tsx
+++ k-site/src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useNavbarStore } from "@/store/navbarStore";
+
 import Hero from "@/components/Landing/Hero";
 import Patronage from "@/components/Landing/Patronage";
 import About from "@/components/Landing/About";
 import Background from "@/components/Background";
 
 export default function Home() {
+  const setHomeNavbar = useNavbarStore((s) => s.setHomeNavbar);
+
+  useEffect(() => {
+    setHomeNavbar();
+  }, []);
+
   return (
     <main
       className="relative min-h-screen overflow-hidden text-white"
