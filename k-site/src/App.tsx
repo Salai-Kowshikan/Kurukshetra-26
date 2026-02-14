d5bd4accf3c2369dd645a7fd39a938ee2f0e4d5f
Sithishwar <sithishwar0507@gmail.com>
2026-02-15 01:16:11 +0530

    workshops page 1

--- k-site/src/App.tsx
+++ k-site/src/App.tsx
@@ -1,69 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
-
-import Home from "@/pages/Home";
-import NotFound from "@/pages/NotFound";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Contact from "@/pages/Contact";
-import ComingSoon from "@/pages/ComingSoon";
-
-const comingSoonPaths = [
-  "/accommodation",
-  "/login",
-  "/register",
-  "/guest-lectures",
-];
-
-// External redirect component
-const ExternalRedirect = ({ url }: { url: string }) => {
-  useEffect(() => {
-    window.location.href = url;
-  }, [url]);
-  return null;
-};
+import Workshops from "./pages/Workshops";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-
-      <Routes>
-        {/* Visible pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-
-        {/* Pages redirected to ComingSoon */}
-        {comingSoonPaths.map((path) => (
-          <Route key={path} path={path} element={<ComingSoon />} />
-        ))}
-
-        {/* External redirects to Unstop */}
-        <Route
-          path="/events"
-          element={
-            <ExternalRedirect url="https://unstop.com/college-fests/kurukshetra-2026-anna-university-ceg-tech-forum-436664" />
-          }
-        />
-        <Route
-          path="/workshops"
-          element={
-            <ExternalRedirect url="https://unstop.com/college-fests/kurukshetra-2026-anna-university-ceg-tech-forum-436664" />
-          }
-        />
-        <Route
-          path="/technovation"
-          element={
-            <ExternalRedirect url="https://unstop.com/p/technovation-kurukshetra-2026-anna-university-ceg-tech-forum-1628748?utm_medium=Share&utm_source=vhcnzgkj55361&utm_campaign=Competitions" />
-          }
-        />
-
-        {/* Catch all - NotFound */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+  return <Workshops />;
 }
 
 export default App;
